d7a9665966846b5ba9c3f5284f0bbb0a1d2d4dad
shayandesh <shafaghi1378@gmail.com>
2021-05-15 00:21:49 +0430

    Added a simple dispatcher

--- safd/router.py
+++ safd/router.py
@@ -7,7 +7,6 @@ class Router:
     def __call__(self, environ, start_response):
         pass
 
-
     def route(self, pattern):
         def decorator(func):
             verb = func.__name__
@@ -16,3 +15,23 @@ class Router:
             regs.append(r)
         return decorator
 
+    def dispatch(self, verb, path):
+        routes = self.routes
+        patterns = routes.get(verb)
+        if not patterns:
+            """
+            TODO: Raise appropriate exception
+            """
+            raise Exception
+        for (pattern, func) in patterns:
+            match = pattern.match(path)
+            if not match:
+                continue
+
+            params = [p for p in match.groups()]
+            return f, params
+
+        """
+        TODO: Raise appropriate exception
+        """
+        raise Exception
