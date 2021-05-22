7377ab1fb9aacb42be98f4d4b6237d6dfe707660
shayandesh <shafaghi1378@gmail.com>
2021-05-23 01:16:06 +0430

    Testing content-type and status decoratrs

--- test/test_application.py
+++ test/test_application.py
@@ -1,7 +1,8 @@
 import webtest
 import pytest
 
-from safd import Application, text
+from safd import Application, text, json, html, code
+from safd.status import BAD_REQUEST
 
 
 def test_route_dispatch():
@@ -36,7 +37,7 @@ def test_route_dispatch():
     f, params = app.dispatch("insert", "/foo/1/baz")
     assert params == ['1'] and f(None, None, *params) == "/foo/id/baz"
 
-def test_requset():
+def test_text_response():
     app = Application()
 
     @app.route("/")
@@ -45,7 +46,48 @@ def test_requset():
         return "Hello World!"
 
     testapp = webtest.TestApp(app)
-
     resp = testapp.get('/')
 
-    assert resp.text == "Hello World!"
+    assert resp.text == "Hello World!" and resp.content_type == "text/plain"
+
+def test_json_response():
+    app = Application()
+
+    @app.route("/")
+    @json
+    def get(request, response):
+        return {"foo": "baz"}
+
+    testapp = webtest.TestApp(app)
+    resp = testapp.get('/')
+
+    assert resp.content_type == "application/json" and resp.json == {"foo": "baz"}
+
+def test_html_response():
+    app = Application()
+
+    @app.route("/")
+    @html
+    def get(request, response):
+        return "<html></html>"
+
+    testapp = webtest.TestApp(app)
+    resp = testapp.get('/')
+    resp_html = resp.html
+    assert resp.content_type == "text/html" and resp.html == resp_html
+
+
+def test_status_code():
+    app = Application()
+
+    @app.route("/")
+    @code(BAD_REQUEST)
+    @text
+    def get(request, response):
+        return "Testing status code"
+
+    testapp = webtest.TestApp(app)
+    resp = testapp.get('/', status="*")
+
+    assert resp.status == "400 BAD REQUEST"
+
